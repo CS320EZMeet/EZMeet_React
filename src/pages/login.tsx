db47fd6eb5e3086c15de024ecb8e7d8f0b4b15a9
Sebastien Thunbo Christensen <seabass6027@gmail.com>
2022-10-22 20:44:37 -0400

    fixing bugs

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,38 +2,46 @@ import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 
 const Login = () => {
-    return (
-        <div className="col-md-12">
-          <div className="card card-container">
-            <img
-              src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-              alt="profile-img"
-              className="profile-img-card"
-            />
-              <Form>
-                <div className="form-group">
-                  <label htmlFor="username">Username</label>
-                  <Field name="username" type="text" className="form-control" />
-                  <ErrorMessage
-                    name="username"
-                    component="div"
-                    className="alert alert-danger"
-                  />
-                </div>
-  
-                <div className="form-group">
-                  <label htmlFor="password">Password</label>
-                  <Field name="password" type="password" className="form-control" />
-                  <ErrorMessage
-                    name="password"
-                    component="div"
-                    className="alert alert-danger"
-                  />
-                </div>
-              </Form>
-          </div>
-        </div>
-      );
-}
+  return (
+    <div className="col-md-12">
+      <div className="card card-container">
+        <Formik
+          initialValues={{
+            username: "username",
+            password: "password",
+          }}
+          onSubmit={(values) => {
+            alert("Submitted");
+          }}
+        >
+          <Form>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <Field name="username" type="text" className="form-control" />
+              <ErrorMessage
+                name="username"
+                component="div"
+                className="alert alert-danger"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <Field name="password" type="password" className="form-control" />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="alert alert-danger"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary btn-block">
+                Login
+            </button>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
