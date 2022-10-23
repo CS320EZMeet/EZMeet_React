9fcfb852b776af0c248f71822b476f783e24764a
Chris Serrao <chrisserrao@vl965-172-31-172-72.wireless.umass.edu>
2022-10-23 19:49:01 -0400

    Navigation Bar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -15,14 +15,38 @@ const Navbar = () => {
     const user_id = 1
     return (
     <nav className="nav-bar">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/help">Help</NavLink>
-        <NavLink to={"/user/" + user_id}>User</NavLink>
-        <NavLink to="/group">Group</NavLink>
-        <NavLink to="/login">Login</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink to="/" className="site-name">EzMeet</NavLink>
+        <ul>
+            <li><NavLink to="/help" className="active">Help</NavLink></li>
+            <li><NavLink to={"/user/" + user_id} className="active">User</NavLink></li>
+            <li><NavLink to="/group" className="active">Group</NavLink></li>
+            <li><NavLink to="/login" className="active">Login</NavLink></li>
+            <li><NavLink to="/signup" className="active">Sign Up</NavLink></li>
+        </ul>
     </nav>
     )
 }
 
 export default Navbar;
+
+
+
+
+
+/*
+const Navbar = () => {
+    const user_id = 1
+    return (
+    <nav className="nav-bar">
+        <a className="site-name"> <NavLink to="/">Home</NavLink> </a>
+        <ul>
+            <li><a><NavLink to="/help">Help</NavLink></a></li>
+            <li><a><NavLink to={"/user/" + user_id}>User</NavLink></a></li>
+            <li><a><NavLink to="/group">Group</NavLink></a></li>
+            <li><a><NavLink to="/login">Login</NavLink></a></li>
+            <li><a><NavLink to="/signup">Sign Up</NavLink></a></li>
+        </ul>
+    </nav>
+    )
+}
+*/
