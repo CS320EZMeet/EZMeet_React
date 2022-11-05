6ddcce35e1114fb7653e95d1ba730d85b4057526
BrianZhang42 <brianzhang42@gmail.com>
2022-11-05 04:06:58 -0400

    login stuff

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,51 +1,3 @@
-// import React from "react";
-// import { Formik, Field, Form, ErrorMessage } from "formik";
-
-// const Login = () => {
-//   return (
-//     <div className="col-md-12">
-//       <div className="card card-container">
-//         <Formik
-//           initialValues={{
-//             username: "username",
-//             password: "password",
-//           }}
-//           onSubmit={(values) => {
-//             alert("Submitted");
-//           }}
-//         >
-//           <Form>
-//             <div className="form-group">
-//               <label htmlFor="username">Username</label>
-//               <Field name="username" type="text" className="form-control" />
-//               <ErrorMessage
-//                 name="username"
-//                 component="div"
-//                 className="alert alert-danger"
-//               />
-//             </div>
-
-//             <div className="form-group">
-//               <label htmlFor="password">Password</label>
-//               <Field name="password" type="password" className="form-control" />
-//               <ErrorMessage
-//                 name="password"
-//                 component="div"
-//                 className="alert alert-danger"
-//               />
-//             </div>
-//             <button type="submit" className="btn btn-primary btn-block">
-//                 Login
-//             </button>
-//           </Form>
-//         </Formik>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
 import { Component } from "react";
 import { useNavigate } from "react-router-dom";
 import { Formik, Field, Form, ErrorMessage } from "formik";
@@ -188,3 +140,48 @@ export default class Login extends Component<Props, State> {
     );
   }
 }
+
+// const Login = () => {
+//   return (
+//     <div className="col-md-12">
+//       <div className="card card-container">
+//         <Formik
+//           initialValues={{
+//             username: "username",
+//             password: "password",
+//           }}
+//           onSubmit={(values) => {
+//             alert("Submitted");
+//           }}
+//         >
+//           <Form>
+//             <div className="form-group">
+//               <label htmlFor="username">Username</label>
+//               <Field name="username" type="text" className="form-control" />
+//               <ErrorMessage
+//                 name="username"
+//                 component="div"
+//                 className="alert alert-danger"
+//               />
+//             </div>
+
+//             <div className="form-group">
+//               <label htmlFor="password">Password</label>
+//               <Field name="password" type="password" className="form-control" />
+//               <ErrorMessage
+//                 name="password"
+//                 component="div"
+//                 className="alert alert-danger"
+//               />
+//             </div>
+//             <button type="submit" className="btn btn-primary btn-block">
+//                 Login
+//             </button>
+//           </Form>
+//         </Formik>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
