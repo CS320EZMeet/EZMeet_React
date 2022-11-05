adce726b8afa78b3e190aeb4dd43eec04942fdf2
Chris Serrao <chrisserrao@vl965-172-31-172-72.wireless.umass.edu>
2022-11-05 16:19:47 -0400

    Basic design for home page

--- src/pages/group 2.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-
-const Group = () => {
-    return (<h1>I'm the group page</h1>)
-}
-
-export default Group;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,49 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { Box, Collapse, CssBaseline, Typography } from "@mui/material";
 
 const Home = () => {
+    const [flag, setFlag] = React.useState(false);
+    useEffect(() => {
+        setFlag(true);
+    }, [])
     return (
-    <div>
-        <h1>Welcome to EzMeet</h1>
-    </div>
+        <Box
+            component="div"
+            sx={{
+                minHeight: '100vh',
+                backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/home.jpeg"})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+            }}
+        >
+            <CssBaseline />
+            <Box
+                component="div"
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alginItems: 'center',
+                    height: '100vh'
+                }}
+            >
+                <Collapse
+                    in={flag}
+                    {...(flag ? { timeout: 1000 } : {})}
+                >
+                    <Typography
+                        mt={30}
+                        variant="h2"
+                        color="#ffffff"
+                        fontWeight="bold"
+                    > Welcome to 
+                        <Typography variant="h2" fontWeight="bold" color="#5aff3d">
+                            EzMeet!
+                        </Typography>
+                    </Typography>
+                </Collapse>
+            </Box>
+        </Box>
     )
-
 }
 
 export default Home;
