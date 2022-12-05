ca1b21f9e4d0bd64393bb343bbdb9ed912ca5ee9
BrianZhang42 <brianzhang42@gmail.com>
2022-12-04 21:13:04 -0500

    fix

--- src/services/authenticator.ts
+++ src/services/authenticator.ts
@@ -14,6 +14,7 @@ class AuthService {
             localStorage.setItem("user", JSON.stringify(response.data));
             console.log(response.data);
         }
+        console.log(response.data);
         return response.data;
         });
     }
